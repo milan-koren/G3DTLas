869274d85c25650e3384c178a843caf6f50a4946
milan-koren <milan.koren3@gmail.com>
2022-02-22 11:05:20 +0100

    Create lasvlrtextarea.h

---
+++ VLR/lasvlrtextarea.h
@@ -0,0 +1,26 @@
+#ifndef LASVLRTEXTAREA_H
+#define LASVLRTEXTAREA_H
+
+#include <QtCore>
+#include "g3dtlas_global.h"
+
+/*!
+ * *****************************************************************
+ *                               G3DTLas
+ * *****************************************************************
+ *
+ * \brief Text area. Optional VLR defined by LAS Specification.
+ *
+ * Text Area
+ * User ID: LASF_Spec
+ * Record ID: 3
+ * Null terminated strings.
+ *
+ * \author M. Koren, milan.koren3@gmail.com
+ * Source: https:\\github.com/milan-koren/G3DTLas
+ * Licence: EUPL v. 1.2
+ * https://joinup.ec.europa.eu/collection/eupl
+ * *****************************************************************
+ */
+
+#endif // LASVLRTEXTAREA_H
