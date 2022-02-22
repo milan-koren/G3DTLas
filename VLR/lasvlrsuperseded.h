bc5c149c8828a5107c8db219603c4d6432e2e3d3
milan-koren <milan.koren3@gmail.com>
2022-02-22 11:05:14 +0100

    Create lasvlrsuperseded.h

---
+++ VLR/lasvlrsuperseded.h
@@ -0,0 +1,27 @@
+#ifndef LASVLRSUPERSEDED_H
+#define LASVLRSUPERSEDED_H
+
+#include <QtCore>
+#include "g3dtlas_global.h"
+
+/*!
+ * *****************************************************************
+ *                               G3DTLas
+ * *****************************************************************
+ *
+ * \brief Superseded record. Optional VLR defined by LAS Specification.
+ *
+ * Superseded
+ * User ID: LASF_Spec
+ * Record ID: 7
+ * Negating the existing VLR.
+ * The existing VLR is identified by Superseded Record ID.
+ *
+ * \author M. Koren, milan.koren3@gmail.com
+ * Source: https:\\github.com/milan-koren/G3DTLas
+ * Licence: EUPL v. 1.2
+ * https://joinup.ec.europa.eu/collection/eupl
+ * *****************************************************************
+ */
+
+#endif // LASVLRSUPERSEDED_H
