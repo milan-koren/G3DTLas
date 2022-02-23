5d1a7e95cd2d934ed0d7ae6a6f2e5ec3b556911e
milan-koren <milan.koren3@gmail.com>
2022-02-23 15:09:36 +0100

    Create lasevlr.cpp

---
+++ EVLR/lasevlr.cpp
@@ -0,0 +1,17 @@
+/*!
+ * *****************************************************************
+ *                               G3DTLas
+ * *****************************************************************
+ * \file lasevlr.cpp
+ *
+ * \brief Extended variable-length record.
+ * \remark There is nothing to implement yet.
+ *
+ * \author M. Koren, milan.koren3@gmail.com
+ * Source: https:\\github.com/milan-koren/G3DTLas
+ * Licence: EUPL v. 1.2
+ * https://joinup.ec.europa.eu/collection/eupl
+ * *****************************************************************
+ */
+
+#include "lasevlr.h"
