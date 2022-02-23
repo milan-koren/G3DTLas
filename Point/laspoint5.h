280c7d40019b093371a5010a0ae5872bd451ad18
milan-koren <milan.koren3@gmail.com>
2022-02-23 15:11:13 +0100

    Update laspoint5.h

--- Point/laspoint5.h
+++ Point/laspoint5.h
@@ -1,13 +1,11 @@
 #ifndef LASPOINT5_H
 #define LASPOINT5_H
 
-#include <QtCore>
-#include "g3dtlas_global.h"
-
 /*!
  * *****************************************************************
  *                               G3DTLas
  * *****************************************************************
+ * \file laspoint5.h
  *
  * \brief Point Data Record Format 5
  *
@@ -17,6 +15,8 @@
  * https://joinup.ec.europa.eu/collection/eupl
  * *****************************************************************
  */
+
+#include "g3dtlas_global.h"
 
 
 #pragma pack(1)
