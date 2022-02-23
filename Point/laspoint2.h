556300bb85cb46ecf10c47271064ade26e7b786f
milan-koren <milan.koren3@gmail.com>
2022-02-23 15:11:18 +0100

    Update laspoint2.h

--- Point/laspoint2.h
+++ Point/laspoint2.h
@@ -1,13 +1,11 @@
 #ifndef LASPOINT2_H
 #define LASPOINT2_H
 
-#include <QtCore>
-#include "g3dtlas_global.h"
-
 /*!
  * *****************************************************************
  *                               G3DTLas
  * *****************************************************************
+ * \file laspoint2.h
  *
  * \brief Point Data Record Format 2
  *
@@ -17,6 +15,8 @@
  * https://joinup.ec.europa.eu/collection/eupl
  * *****************************************************************
  */
+
+#include "g3dtlas_global.h"
 
 
 #pragma pack(1)
